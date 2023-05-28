5d0edbf8ae623c7c589c84dbab11729bdff4dbce
Ahmet Berkay Yılmaz <134816130+ahmetberkay54@users.noreply.github.com>
2023-05-28 04:36:57 +0300

    Add project files.

---
+++ Project7/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Nereden : " + comboBox1.Text + " Nereye : " + comboBox2.Text + " Tarih : " + dateTimePicker1.Text + " Saat : " + maskedTextBox1.Text + " Yolcu Adı ve Soyad : " + textBox1.Text + " TCKN : " + maskedTextBox2.Text + " Telefon Numarası : " + maskedTextBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string X;
+            X = comboBox1.Text;
+            comboBox1.Text = comboBox2.Text;
+            comboBox2.Text = X;
+
+            int a, b;
+            a = 7; b = 3;
+            MessageBox.Show(a / b);
+
+
+
+
+
+        }
+    }
+}
